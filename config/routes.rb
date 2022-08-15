193ce5bc6cca67b8e3c2839d9d86976230eba49b
Franco Rivera <franco@LAPTOP-7R15A7O9.localdomain>
2022-08-15 21:29:09 +0800

    Add edit, update, and delete to articles database

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'new_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+  #unsure code
+  get '/articles/edit/:id' => 'articles#edit', as: 'edit_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/articles/:id' => 'articles#delete', as: 'delete_article'
 end
